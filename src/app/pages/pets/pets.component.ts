91cef6ac9a3060899342456b659b586ae1294978
AK-macbook <aalkhareji27@gmail.com>
2025-04-24 12:28:55 +0300

    Refactor pets.component.ts by removing unused imports and the AsyncPipe from the component's imports, streamlining the code for better performance and readability.

--- src/app/pages/pets/pets.component.ts
+++ src/app/pages/pets/pets.component.ts
@@ -1,16 +1,14 @@
-import { Component, computed, effect, inject, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { PetsHeaderComponent } from '../../components/pets-header/pets-header.component';
 import { PetsListComponent } from '../../components/pets-list/pets-list.component';
 import { Pet } from '../../../data/pets';
 import { PetService } from '../../services/pet.service';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { map, Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-pets',
   standalone: true,
-  imports: [PetsHeaderComponent, PetsListComponent, AsyncPipe],
+  imports: [PetsHeaderComponent, PetsListComponent],
   templateUrl: './pets.component.html',
   styleUrl: './pets.component.css',
 })
